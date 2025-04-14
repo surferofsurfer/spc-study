f03e97652cba01af581125d29a0f32d1316eebc8
김병희 <surfersurferlife93@gmail.com>
2025-04-14 23:24:09 +0900

    node.js 에서 파일을 읽는법, 쓰는법에 대해 학습

---
+++ app.js
@@ -0,0 +1,11 @@
+function addNumbers (a, b) {
+    return a+b;
+}
+
+function subNumbers (a,b) {
+    return a-b;
+}
+
+module.exports = { 
+    addNumbers, subNumbers };
+// 이 함수 갖다쓰라는 말
---
+++ index.js
@@ -0,0 +1,8 @@
+const {addNumbers, subNumbers} = require ('./app');
+
+
+let result = addNumbers (2,3);
+console.log(result);
+
+result = subNumbers (2,3);
+console.log(result);
